f00da5690c5eef108d9ba9a920f0948030659d53
Ben <ben@ahnlab.io>
2022-11-23 21:43:23 +0900

    webp image add type

--- s3img/app.py
+++ s3img/app.py
@@ -1,11 +1,11 @@
 import logging
+import mimetypes
 import pathlib
 import tempfile
-import boto3
-import uuid
 from urllib.parse import unquote_plus
+
+import boto3
 import imgtool
-import mimetypes
 
 s3_client = boto3.client("s3")
 
@@ -22,10 +22,20 @@ def lambda_handler(event, context):
         download_path = "/tmp/{}".format(tmpkey)
         s3_client.download_file(bucket, key, download_path)
 
+        mimetypes.add_type("image/webp", ".webp")
         mime_type, _ = mimetypes.guess_type(download_path)
+        # mime_type = magic.from_file(download_path, mime=True)
         if mime_type is None or not mime_type.startswith("image/"):
             log.info("mimetype=%s. skip.", mime_type)
             return
+
+        # 원본 resized bucket 에 복사
+        s3_client.upload_file(
+            Filename=download_path,
+            Bucket="{}-resized".format(bucket),
+            Key=pathlib.Path(download_path).name,
+            ExtraArgs={"ContentType": mime_type},
+        )
 
         with tempfile.TemporaryDirectory() as dir:
             images = imgtool.make_thumbnails(
@@ -33,8 +43,8 @@ def lambda_handler(event, context):
             )
             for img_path in images:
                 s3_client.upload_file(
-                    str(img_path),
-                    "{}-resized".format(bucket),
-                    img_path.name,
+                    Filename=str(img_path),
+                    Bucket="{}-resized".format(bucket),
+                    Key=img_path.name,
                     ExtraArgs={"ContentType": mime_type},
                 )
